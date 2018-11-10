d30299053d6ffc9a795f60612b18e163328022f7
bradford lamson-scribner <brad.lamson@gmail.com>
2018-11-09 20:36:29 -0800

    gql types

---
+++ gql/types.go
@@ -0,0 +1,27 @@
+package gql
+
+import "github.com/graphql-go/graphql"
+
+// User is a graphql object containing a User
+var User = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "User",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+			"age": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"profession": &graphql.Field{
+				Type: graphql.String,
+			},
+			"friendly": &graphql.Field{
+				Type: graphql.Boolean,
+			},
+		},
+	},
+)
