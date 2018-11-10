e819684b5b2150230ea9a3f42db62897eefdf86d
bradford lamson-scribner <brad.lamson@gmail.com>
2018-11-09 20:35:54 -0800

    main.go

--- main.go
+++ main.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/bradford-hamilton/go-graphql-api/gql"
+	"github.com/bradford-hamilton/go-graphql-api/postgres"
+	"github.com/bradford-hamilton/go-graphql-api/server"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+	"github.com/graphql-go/graphql"
+)
+
+func main() {
+	router, db := initializeAPI()
+	defer db.Close()
+
+	log.Fatal(http.ListenAndServe(":4000", router))
+}
+
+func initializeAPI() (*chi.Mux, *postgres.Db) {
+	router := chi.NewRouter()
+
+	db, err := postgres.New(
+		postgres.ConnString("localhost", 5432, "bradford", "go_test_db"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rootQuery := gql.NewRoot(db)
+	sc, err := graphql.NewSchema(
+		graphql.SchemaConfig{Query: rootQuery.Query},
+	)
+	if err != nil {
+		fmt.Println("Error creating schema: ", err)
+	}
+
+	s := server.Server{
+		Db:        db,
+		GqlSchema: &sc,
+	}
+
+	router.Use(
+		render.SetContentType(render.ContentTypeJSON), // set content-type headers as application/json
+		middleware.Logger,          // log api request calls
+		middleware.DefaultCompress, // compress results, mostly gzipping assets and json
+		middleware.StripSlashes,    // match paths with a trailing slash, strip it, and continue routing through the mux
+		middleware.Recoverer,       // recover from panics without crashing server
+	)
+
+	// GraphQL route
+	router.Post("/graphql", s.GraphQL())
+
+	// Restful routes
+	router.Get("/restful/endpoint", s.RestfulEndpoint())
+
+	return router, db
 }
