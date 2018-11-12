4210f6db7cf405028406f44b5cc4e7de8048213f
bradford lamson-scribner <brad.lamson@gmail.com>
2018-11-11 20:15:06 -0700

    small updates and comments

--- gql/gql.go
+++ gql/gql.go
@@ -3,19 +3,18 @@ package gql
 import (
 	"fmt"
 
-	"github.com/bradford-hamilton/go-graphql-api/postgres"
 	"github.com/graphql-go/graphql"
 )
 
-// ExecuteQuery runs a graphql query
-func ExecuteQuery(query string, schema graphql.Schema, db *postgres.Db) *graphql.Result {
+// ExecuteQuery runs our graphql queries
+func ExecuteQuery(query string, schema graphql.Schema) *graphql.Result {
 	result := graphql.Do(graphql.Params{
 		Schema:        schema,
 		RequestString: query,
 	})
 
 	if len(result.Errors) > 0 {
-		fmt.Printf("wrong result, unexpected errors: %v", result.Errors)
+		fmt.Printf("Unexpected errors inside ExecuteQuery: %v", result.Errors)
 	}
 
 	return result
--- server/server.go
+++ server/server.go
@@ -23,18 +23,21 @@ type reqBody struct {
 // GraphQL returns an http.HandlerFunc for our /graphql endpoint
 func (s *Server) GraphQL() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Check to ensure query was provided in the request body
 		if r.Body == nil {
 			http.Error(w, "Must provide graphql query in request body", 400)
 			return
 		}
 
 		var rBody reqBody
+		// Decode the request body into rBody
 		err := json.NewDecoder(r.Body).Decode(&rBody)
 		if err != nil {
 			http.Error(w, "Error parsing JSON request body", 400)
 		}
 
-		result := gql.ExecuteQuery(rBody.Query, *s.GqlSchema, s.Db)
+		// Execute graphql query
+		result := gql.ExecuteQuery(rBody.Query, *s.GqlSchema)
 		render.JSON(w, r, result)
 	}
 }
