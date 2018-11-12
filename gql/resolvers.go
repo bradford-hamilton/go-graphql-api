5989187fee3702247a0520717f100ee5ea01eedd
bradford lamson-scribner <brad.lamson@gmail.com>
2018-11-11 19:50:01 -0700

    add comments to resolvers.go for tutorial

--- gql/resolvers.go
+++ gql/resolvers.go
@@ -10,12 +10,13 @@ type Resolver struct {
 	db *postgres.Db
 }
 
-// UserResolver resolves our person query through a db call to GetUserByName
+// UserResolver resolves our user query through a db call to GetUserByName
 func (r *Resolver) UserResolver(p graphql.ResolveParams) (interface{}, error) {
+	// Strip the name from arguments and assert that it's a string
 	name, ok := p.Args["name"].(string)
 	if ok {
-		block := r.db.GetUserByName(name)
-		return block, nil
+		user := r.db.GetUserByName(name)
+		return user, nil
 	}
 
 	return nil, nil
