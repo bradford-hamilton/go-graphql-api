53e4cfad65deed6bcf4dfd30b1508fe62dfff6e1
bradford lamson-scribner <brad.lamson@gmail.com>
2018-11-11 20:24:07 -0700

    small update

--- gql/gql.go
+++ gql/gql.go
@@ -13,6 +13,7 @@ func ExecuteQuery(query string, schema graphql.Schema) *graphql.Result {
 		RequestString: query,
 	})
 
+	// Error check
 	if len(result.Errors) > 0 {
 		fmt.Printf("Unexpected errors inside ExecuteQuery: %v", result.Errors)
 	}
